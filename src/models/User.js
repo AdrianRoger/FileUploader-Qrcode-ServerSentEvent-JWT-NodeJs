3b0cd33ea02f8169b3988664d6b979b484f8390e
Adrian Roger <adrianroge@gmail.com>
2024-04-21 16:26:35 -0300

    [feat]: create User model

---
+++ src/models/User.js
@@ -0,0 +1,10 @@
+class User {
+  constructor({ id, username, password, type }) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.type = type;
+  }
+}
+
+module.exports = User;
